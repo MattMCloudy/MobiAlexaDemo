1711c3b73e0db440b606f11e732c8c6a5974f636
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-04-03 14:53:43 -0500

    arrow function breaks this.emit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Alexa = require('alexa-sdk');
 const SC = require('string_constants');
 
-const APP_ID = ''
+const APP_ID = 'amzn1.ask.skill.e3fb2dfa-12eb-4ba0-ab3b-f4561956a28a'
 
 const handlers = {
-    'LaunchRequest': () => {
-        this.emit(':tell', SC.WELCOME_MESSAGE)
+    'LaunchRequest': function () {
+        this.emit(':tell', SC.WELCOME_MESSAGE);
     }
 }
 
